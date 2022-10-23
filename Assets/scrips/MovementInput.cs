e494f0e97a0636b00002268bc26f1dd873ef0d22
YoAdro <alesaurus29@gmail.com>
2022-10-22 19:14:10 -0700

    mover
    le doy movimiento

---
+++ Assets/scrips/MovementInput.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+
+    public CharacterController player;
+
+    public float playerSpeed;
+    private Vector3 movePlayer;
+
+    public Camera camera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * playerSpeed * Time.deltaTime);
+
+        Debug.Log(player.velocity.magnitude);
+    }
+
+    void camDirection()
+    {
+        camForward = camera.transform.forward;
+        camRight = camera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+}
